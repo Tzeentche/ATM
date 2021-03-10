4bc3fd19be18d054c54ef3608a7fd056ac0155b5
Tzeentche <ilyasugako87@gmail.com>
2021-03-10 23:17:47 +0300

    Refactored UsersInteractive class.

--- Calculus/src/UsersInteractive.java
+++ Calculus/src/UsersInteractive.java
@@ -1,2 +1,28 @@
+
+import java.util.*;
+
 public class UsersInteractive {
+
+    Operator operator = new Operator();
+
+    public void letsDance() {
+        String yesNo = "Y";
+
+        while ("Y".equals(yesNo)) {
+            double value1 = operator.readNumber("Enter value 1: ");
+            double value2 = operator.readNumber("Enter value 2: ");
+
+            System.out.println("Value 1 = " + value1);
+            System.out.println("Value 2 = " + value2);
+
+            System.out.println("Result " + operator.doOperation(value1, value2));
+
+            yesNo = operator.readString("Continue? (Y\\N)?");
+
+            if (!"Y".equals(yesNo) || !"y".equals(yesNo)) {
+
+                System.exit(0);
+            }
+        }
+    }
 }
