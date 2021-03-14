7a8c239a344b875297600c5e0b07ac4898f31b72
Tzeentche <ilyasugako87@gmail.com>
2021-03-14 21:00:26 +0300

    Refactored InputWords class.

--- CommonLangAndMaven/src/main/java/InputWords.java
+++ CommonLangAndMaven/src/main/java/InputWords.java
@@ -1,2 +1,23 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class InputWords {
+
+    public String[] wordsFilling() {
+
+        String[] lablesArray = new String[10];
+
+        for(int i = 0; i < 10; i++) {
+
+            lablesArray[i] = RandomStringUtils.randomAlphabetic(10);
+        }
+
+
+        for (int i = 0; i < lablesArray.length; i++) {
+
+            System.out.println("Word is " + lablesArray[i]);
+        }
+
+        return lablesArray;
+    }
+
 }
