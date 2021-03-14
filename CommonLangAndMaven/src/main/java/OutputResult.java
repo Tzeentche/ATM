8aec3543e3e8399a4c7115efa639f22bdc936039
Tzeentche <ilyasugako87@gmail.com>
2021-03-14 20:59:55 +0300

    Refactored OutputResult class.

--- CommonLangAndMaven/src/main/java/OutputResult.java
+++ CommonLangAndMaven/src/main/java/OutputResult.java
@@ -1,2 +1,10 @@
 public class OutputResult {
+
+
+        public void outputter(String[] innerDate) {
+
+            for (int i = 0; i < innerDate.length; i++) {
+                System.out.println("Final result is: " + innerDate[i]);
+            }
+        }
 }
