a2c7e5cc77167868f1203106199f04b311f716ad
Tzeentche <ilyasugako87@gmail.com>
2021-03-14 20:59:35 +0300

    Refactored WordsConverter class.

--- CommonLangAndMaven/src/main/java/WordsConverter.java
+++ CommonLangAndMaven/src/main/java/WordsConverter.java
@@ -1,4 +1,30 @@
+
+import java.lang.*;
+import org.apache.commons.lang3.*;
+
 public class WordsConverter {
 
-    
+    InputWords inputWords = new InputWords();
+    String[] reformedArray = inputWords.wordsFilling();
+
+        public String[] capitalizer() {
+
+
+            String[] capitalizeArray = reformedArray;
+
+                for(int i = 0; i < capitalizeArray.length; i++) {
+
+                    capitalizeArray[i] = StringUtils.capitalize(capitalizeArray[i]);
+                }
+            return capitalizeArray;
+        }
+
+    public String[] leverer() {
+
+        for(int i = 0; i < reformedArray.length; i++) {
+
+            reformedArray[i] = reformedArray[i].toLowerCase();
+        }
+        return reformedArray;
+    }
 }
