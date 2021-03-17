428dcc1d4b2b9927539572123348f50128d3db4c
Tzeentche <ilyasugako87@gmail.com>
2021-03-18 00:26:59 +0300

    Create OperatorTest class.

---
+++ Calculus/src/test/java/OperatorTest.java
@@ -0,0 +1,26 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperatorTest {
+
+    @org.junit.jupiter.api.Test
+    void readIntNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readDoubleNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void doOperation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectOperation() {
+    }
+}
