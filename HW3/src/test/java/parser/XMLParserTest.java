b515b6073c6cdce70bbe72f8d7db7c37f9f7ae5a
Tzeentche <ilyasugako87@gmail.com>
2021-03-29 01:51:00 +0300

    Create Samples and run Parser.

--- HW3/src/test/java/parser/XMLParserTest.java
+++ HW3/src/test/java/parser/XMLParserTest.java
@@ -6,13 +6,15 @@ import static org.testng.Assert.*;
 
 public class XMLParserTest {
 
-//    XMLParser xmlParser = new XMLParser();
+    XMLParser xmlParser = new XMLParser("C:\\Users\\Horus\\Sugako\\HW3\\src\\xmlFiles\\Sample4.xml");
 
     @Test
     public void testGetErrors() {
+        xmlParser.getErrors();
     }
 
     @Test
     public void testParseDocument() {
+        xmlParser.parseDocument();
     }
 }
