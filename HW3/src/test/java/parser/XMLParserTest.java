633944ca2b8b274b0ef7225e43a11378c6319925
Tzeentche <ilyasugako87@gmail.com>
2021-03-28 19:57:59 +0300

    Start HW3

---
+++ HW3/src/test/java/parser/XMLParserTest.java
@@ -0,0 +1,18 @@
+package parser;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class XMLParserTest {
+
+//    XMLParser xmlParser = new XMLParser();
+
+    @Test
+    public void testGetErrors() {
+    }
+
+    @Test
+    public void testParseDocument() {
+    }
+}
