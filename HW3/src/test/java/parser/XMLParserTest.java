70ece4eaa4207f397818fc04ee4040bb2beb9f3f
Tzeentche <ilyasugako87@gmail.com>
2021-03-30 15:25:38 +0300

    Refactored XMLParserTest class: add object, path to document, output message.

--- HW3/src/test/java/parser/XMLParserTest.java
+++ HW3/src/test/java/parser/XMLParserTest.java
@@ -1,20 +1,34 @@
 package parser;
 
+import org.apache.tools.ant.taskdefs.condition.IsTrue;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ListADT;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class XMLParserTest {
 
-    XMLParser xmlParser = new XMLParser("C:\\Users\\Horus\\Sugako\\HW3\\src\\xmlFiles\\Sample4.xml");
-
-    @Test
-    public void testGetErrors() {
-        xmlParser.getErrors();
-    }
+    XMLParser xmlParser;
 
     @Test
     public void testParseDocument() {
+        ListADT<String> list;
+        xmlParser = new XMLParser("C:\\Users\\Horus\\Sugako\\HW3\\src\\xmlFiles\\Sample10.xml");
         xmlParser.parseDocument();
+        list = xmlParser.getErrors();
+        if(list.isEmpty()) {
+            Assert.assertTrue(true);
+
+        } else{
+            for(int i = 0; i < list.size(); i++) {
+                System.out.println("What's happened: " + list.get(i));
+            }
+            Assert.assertTrue(false);
+        }
+
     }
 }
