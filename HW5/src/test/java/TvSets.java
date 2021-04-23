16cf890fb7c26c8225b8445cf0b23754da101ac0
Tzeentche <ilyasugako87@gmail.com>
2021-04-23 17:18:39 +0300

    1. Add window maximize. 2. Add loadPage waiter. 3. Replaced substring.

--- HW5/src/test/java/TvSets.java
+++ HW5/src/test/java/TvSets.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 public class TvSets {
 
     static WebDriver driver;
+    static WebDriverWait wait;
     static JavascriptExecutor js;
 
         @BeforeAll
@@ -28,6 +31,7 @@ public class TvSets {
         @Test
         public void test() throws InterruptedException {
 
+            driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
             driver.findElement(By.xpath("//span[@class=\"b-main-navigation__text\" and text()='Каталог']")).click();
             driver.findElement(By.xpath("//span[@class=\"catalog-navigation-classifier__item-title-wrapper\" and text()='Электроника']")).click();
@@ -36,6 +40,8 @@ public class TvSets {
             driver.findElement(By.xpath("//input[@value='lg']/following-sibling::span")).click();
             Assertions.assertTrue(driver.findElement(By.xpath("//input[@value='lg']")).isSelected());
             Assertions.assertFalse(driver.findElement(By.xpath("//input[@value='samsung']")).isSelected());
+            driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+            driver.findElement(By.xpath("//span[@data-bind=\"text: $root.nextPageText\" and text()='Следующие 30 товаров']"));
 
             List<WebElement> fullName = new ArrayList<>();
             fullName = driver.findElements(By.xpath("//span[@data-bind=\"html: product.extended_name || product.full_name\"]"));
@@ -43,7 +49,7 @@ public class TvSets {
             for(WebElement webElement : fullName) {
                 try {
                     System.out.println("FullName= " + webElement.getText().substring(9, 20));
-                    Assertions.assertTrue(webElement.getText().toLowerCase().substring(9, 20).contains("lg"));
+                    Assertions.assertTrue(webElement.getText().toLowerCase().contains("lg"));
 
                 } catch (Exception e) {
                     e.getMessage();
