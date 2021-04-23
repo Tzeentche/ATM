3419f3f6f59813ac3d55b893e5265241335e237a
Tzeentche <ilyasugako87@gmail.com>
2021-04-23 15:16:08 +0300

    Refactored HW5 - add Assertions.

--- HW5/src/test/java/TvSets.java
+++ HW5/src/test/java/TvSets.java
@@ -14,17 +14,19 @@ import java.util.concurrent.TimeUnit;
 
 public class TvSets {
 
-    WebDriver driver = new ChromeDriver();
+    static WebDriver driver;
+    static JavascriptExecutor js;
 
         @BeforeAll
         public static void init() {
             System.setProperty("webdriver.chrome.driver", ".\\src\\chromedriver1.exe");
+            driver = new ChromeDriver();
+            js = (JavascriptExecutor) driver;
+            driver.get("https://onliner.by");
         }
 
         @Test
         public void test() throws InterruptedException {
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            driver.get("https://onliner.by");
 
             driver.findElement(By.xpath("//span[@class=\"b-main-navigation__text\" and text()='Каталог']")).click();
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -35,15 +37,17 @@ public class TvSets {
             js.executeScript("window.scrollBy(0, 5000)");
             driver.findElement(By.xpath("//input[@value='lg']/following-sibling::span")).click();
             driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+            
             Assertions.assertTrue(driver.findElement(By.xpath("//input[@value='lg']")).isSelected());
-//            Assertions.assertFalse(driver.findElement(By.xpath("//input[@value='samsung']")).isSelected());
+            Assertions.assertFalse(driver.findElement(By.xpath("//input[@value='samsung']")).isSelected());
 
             List<WebElement> fullName = new ArrayList<>();
             fullName = driver.findElements(By.xpath("//span[@data-bind=\"html: product.extended_name || product.full_name\"]"));
 
             for(WebElement webElement : fullName) {
                 try {
-                    System.out.println("fullName = " + webElement.getText());
+                    System.out.println("FullName= " + webElement.getText().substring(9, 20));
+                    Assertions.assertTrue(webElement.getText().toLowerCase().substring(9, 20).contains("lg"));
 
                 } catch (Exception e) {
                     e.getMessage();
